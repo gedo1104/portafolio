40ab73e83b400059503442346a995de9888deeff
Gerardo Ramirez <gedrix.rm93@gmail.com>
2021-05-19 18:41:02 -0500

    arreglos

--- js/main.js
+++ js/main.js
@@ -46,17 +46,13 @@ sr.reveal('.about__subtitle',{delay: 400});
 sr.reveal('.about__text',{delay: 400}); 
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
+// sr.reveal('.skills__subtitle',{interval: 200}); 
+sr.reveal('.skills__text',{interval: 200}); 
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
-
-/*SCROLL CONTACT*/
-// sr.reveal('.contact__input',{interval: 200}); 
-
 
 /*DARK MODE */
 var icon = document.getElementById("icon");
