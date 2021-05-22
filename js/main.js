8a2b075d67be4aed334f6c75765dc8d15ebc5700
Gerardo Ramirez <gedrix.rm93@gmail.com>
2021-05-22 16:20:21 -0500

    doble idioma

--- js/main.js
+++ js/main.js
@@ -99,3 +99,24 @@ icon.onclick = function (){
 	}
 }
 
+/*traducir */
+$.getJSON("js/lang.json", function(json){
+    
+    if(!localStorage.getItem("lang")){
+      localStorage.setItem("lang", "en");
+    }
+    var lang = localStorage.getItem("lang");
+    var doc = json;
+    $('.lang').each(function(index, element){
+      $(this).text(doc[lang][$(this).attr('key')]);
+    });//Each
+  
+    $('.translate').click(function(){
+      localStorage.setItem("lang", $(this).attr('id')) ;
+      var lang = $(this).attr('id');
+      var doc = json;
+        $('.lang').each(function(index, element){
+          $(this).text(doc[lang][$(this).attr('key')]);
+        }); //Each
+    }); //Funcion click
+  });//Get json AJAX
